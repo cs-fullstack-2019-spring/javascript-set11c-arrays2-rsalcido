16ca82512df85f43e69539981826175d2bd84ef2
Ricardo Salcido <rsalcido@liberty.edu>
2019-01-15 00:42:36 -0600

    classworkweek2

--- Problem33.js
+++ Problem33.js
@@ -3,17 +3,15 @@
 // Ask the user to enter a word to add to an array. Add the user input into the array.
 //     If the user enters a 'q', stop asking them to enter a word.
 
-
-
-word= parseInt(prompt("Enter word to add to the array."));
+x=[];
 
 do {
-    word == word;
+    word= prompt("Enter word to add to the array or enter 'q' to stop.");
+    x.push(word)
+} while(word !== "q");
 
 
-}while (word !== "q") ;
-
-
-
-
-
+for (i=0; i<x.length; i++) {
+    console.log("The word is: " + x[i]);
+    //console.log("The value of i is: "+i)
+}
